1b4729815833d30ccf3527707af1970201a3effa
Gabriel <gvogt@asu.edu>
2025-01-29 03:07:45 -0700

    Add project files.

---
+++ AlienAdminSystem/Alien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienAdminSystem
+{
+    internal class Alien
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Planet { get; set; } = string.Empty;
+
+        public string Image { get; set; } = string.Empty;
+
+        public int Age { get; set; } = 0;
+
+    }
+}
---
+++ AlienAdminSystem/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AlienAdminSystem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

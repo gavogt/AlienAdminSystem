9d0ba0a9e8c75873cdaf5eb20302ccc32fd4f809
Gabriel <gvogt@asu.edu>
2025-03-19 11:52:08 -0700

    Added Embassy and Quarantine Zone page, populated them on the navbar. Added skeleton for Sign Up work

---
+++ AlienAdminSystem/SignUpDatabaseService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienAdminSystem
+{
+    internal class SignUpDatabaseService
+    {
+      
+    }
+}
+
---
+++ AlienAdminSystem/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienAdminSystem
+{
+    internal class User
+    {
+
+        public string Username { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+
+    }
+}

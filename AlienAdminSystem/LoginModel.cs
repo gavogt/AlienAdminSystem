5c8ec83a3937d8a8b8ce9c817679713564abdb41
Gabriel <gvogt@asu.edu>
2025-03-20 11:05:39 -0700

    set up a functional loginpage that doesn't redirect

--- AlienAdminSystem/LoginModel.cs
+++ AlienAdminSystem/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace AlienAdminSystem
     internal class LoginModel
     {
 
+        [Required(ErrorMessage = "Username is required.")]
         public string Username { get; set; } = String.Empty;
+        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; } = String.Empty;
 
     }

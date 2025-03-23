6a1126ad595a4ac55ba2a390829e7e1313fbe4c7
Gabriel <gvogt@asu.edu>
2025-03-23 12:49:04 -0700

    Created a Facility Status Management page where Admins can set Facility Status to Open or Undermaintenance as per requirements

--- AlienAdminSystem/AlienInputModel.cs
+++ AlienAdminSystem/AlienInputModel.cs
@@ -11,22 +11,22 @@ namespace AlienAdminSystem
     internal class AlienInputModel
     {
         [Required(ErrorMessage = "First name is required.")]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = String.Empty;
         [Required(ErrorMessage = "Last name is required.")]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = String.Empty;
         [Required(ErrorMessage = "Planet is required.")]
-        public string Planet { get; set; }
+        public string Planet { get; set; } = String.Empty;
         [Required(ErrorMessage = "Species is required.")]
         public Species Species { get; set; }
         [Required(ErrorMessage = "Email is required.")]
-        public string Email { get; set; }
+        public string Email { get; set; } = String.Empty;
         [Range(0, 1000, ErrorMessage = "Age must be between 0 and 1000.")]
         public int Age { get; set; }
         [Required(ErrorMessage = "Group ID is required.")]
         public int AlienGroupID { get; set; }
         [Range(0, 2, ErrorMessage = "Atmosphere type must be between 0 and 2.")]
         public int AtmosphereTypeID { get; set; }
-        public string SpecialRequirements { get; set; }
+        public string SpecialRequirements { get; set; } = String.Empty;
         [Range(0, 12, ErrorMessage = "Visit duration must be between 0 and 12 months.")]
         public int VisitDurationMonths { get; set; }
     }
--- AlienAdminSystem/FacilityDatabaseService.cs
+++ AlienAdminSystem/FacilityDatabaseService.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlienAdminSystem
 {
     internal class FacilityDatabaseService
     {
+        private readonly AlienDBContext _dbContext;
+
+        public FacilityDatabaseService(AlienDBContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<List<Facility>> GetFacilitiesASync()
+        {
+            return await _dbContext.Facilities.ToListAsync();
+        }
+
+        public async Task UpdateFacilityStatusAsync(int ID, Status newStatus)
+        {
+            var facility = await _dbContext.Facilities.FindAsync(ID);
+            if (facility != null)
+            {
+                facility.Status = newStatus;
+                _dbContext.Facilities.Update(facility);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
     }
 }

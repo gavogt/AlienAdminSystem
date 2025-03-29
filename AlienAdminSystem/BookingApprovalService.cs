27b9edbab91f60cc940a93d179db31c44829bdb7
Gabriel <gvogt@asu.edu>
2025-03-29 09:24:01 -0700

    Added a AdminBookingApproval page where admins can change booking status to a number of different booking statuses

--- AlienAdminSystem/BookingApprovalService.cs
+++ AlienAdminSystem/BookingApprovalService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlienAdminSystem
 {
@@ -29,6 +30,23 @@ namespace AlienAdminSystem
             }
         }
 
+        public async Task<List<BookingApproval>> GetBookingApprovalAsync()
+        {
+            return await _context.BookingApproval.ToListAsync();
 
+        }
+
+        public async Task UpdateBookingApprovalAsync(BookingApproval bookingApproval)
+        {
+            try
+            {
+                _context.BookingApproval.Update(bookingApproval);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
     }
 }
--- AlienAdminSystem/BookingStatus.cs
+++ AlienAdminSystem/BookingStatus.cs
@@ -14,6 +14,7 @@ namespace AlienAdminSystem
             AutoApproved,
             Rejected,
             PendingApproval,
+            Approved,
 
         }
     }
